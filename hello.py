50c1129db24d674e7e6ce6bb368d1bcf9ec5a75c
Malini Agrawal <107155358+MaliniAgrawal@users.noreply.github.com>
2023-03-05 10:37:54 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,3 +15,27 @@ items = {"base_cost": 100, "shipping_cost": 10, "tax_rate": 0.10}
 
 total_cost = calculate_total_cost(items)
 print(total_cost)
+
+# code to see comparison for double two char in two string
+
+def string_match(a, b):
+    count = 0
+    for i in range(len(a)-1):
+        if a[i:i+2] == b[i:i+2]:
+            count += 1
+    return count
+#OR same code in better way
+ def string_match(a, b):
+    # Figure which string is shorter.
+    shorter = min(len(a), len(b))
+    count = 0
+
+    # Loop i over every substring starting spot.
+    # Use length-1 here, so can use char str[i+1] in the loop
+    for i in range(shorter - 1):
+        a_sub = a[i:i + 2]
+        b_sub = b[i:i + 2]
+        if a_sub == b_sub:
+            count = count + 1
+
+    return count
